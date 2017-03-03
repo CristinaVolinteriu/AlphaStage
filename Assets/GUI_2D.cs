e64f52a39b15d04fade110176a30063ae87fd83a
CristinaVolinteriu <cristinavolinteriu@gmail.com>
2017-03-03 17:21:01 +0200

    2D GUI Done, started 3D

---
+++ Assets/GUI_2D.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_2D : MonoBehaviour {
+
+    List<Rect> SkillButtons = new List<Rect>();
+    List<Rect> ItemButtons = new List<Rect>();
+
+    // Variables used to calculate the health and the length of the bar
+    public float currentHP = 100;
+    public float maxHP = 100;
+    public float currentBarLength;
+    public float maxBarLength = 100;
+
+    // Level counter
+    public int currentLevel = 1;
+    public GUIStyle myStyle; // Used to access the properties of a GUI label
+
+    // Creating an experience counter
+    public float maxExperience = 100;
+    public float currentExperience = 0;
+    public float curretExpBarLength;
+    public float maxExpBarLength = 100;
+
+	// Use this for initialization
+	void Start () {
+        SkillButtons.Add(new Rect(Screen.width / 2 + 50, Screen.height / 2 + 333, 55, 55));
+        SkillButtons.Add(new Rect(Screen.width / 2 + 105, Screen.height / 2 + 333, 55, 55));
+        SkillButtons.Add(new Rect(Screen.width / 2 + 160, Screen.height / 2 + 333, 55, 55));
+
+        ItemButtons.Add(new Rect(Screen.width / 2 - 160, Screen.height / 2 + 333, 55, 55));
+        ItemButtons.Add(new Rect(Screen.width / 2 - 105, Screen.height / 2 + 333, 55, 55));
+        ItemButtons.Add(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 333, 55, 55));
+
+        myStyle.fontSize = 36;
+    }
+
+    private void OnGUI()
+    {
+        // They draw our button at the location of our rectangles
+        GUI.Button(SkillButtons[0], "Skill A");
+        GUI.Button(SkillButtons[1], "Skill B");
+        GUI.Button(SkillButtons[2], "Skill C");
+        GUI.Button(ItemButtons[0], "Item A");
+        GUI.Button(ItemButtons[1], "Item B");
+        GUI.Button(ItemButtons[2], "Item C");
+
+        // Drawing the health bar
+        currentBarLength = currentHP * maxBarLength / maxHP;
+        GUI.Box(new Rect(Screen.width / 2 - 20, Screen.height / 2 + 300, currentBarLength, 25f), "");
+
+        // Drawing the level counter
+        GUI.Label(new Rect(Screen.width / 2 + 15, Screen.height / 2 + 335, 30, 30), currentLevel.ToString(), myStyle);
+
+        // Drawing an experience counter
+        curretExpBarLength = currentExperience * maxExpBarLength / maxExperience;
+        if(curretExpBarLength > 5)
+        {
+            GUI.Button(new Rect(Screen.width / 2 - 20, Screen.height / 2 - 300, curretExpBarLength, 25), "");
+        }
+        else
+        {
+            GUI.Button(new Rect(Screen.width / 2 - 20, Screen.height / 2 - 300, maxExperience, 25), "");
+        }
+        if(curretExpBarLength >= maxExpBarLength)
+        {
+            curretExpBarLength = 0;
+            currentExperience = 0;
+            currentLevel++;
+        }
+    }
+}
---
+++ Assets/GUI_3D.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_3D : MonoBehaviour {
+
+    // Creating a 3D Health bar
+    public float currentHealth = 100;
+    public float maximumHealth = 100;
+    float currentBarLength;
+    public Transform HealthBar; // Used to interact with our 3D object that's being used as a health bar;
+    Vector3 OrigScale; // Used for scaling
+
+    // Creating 3D damage reports
+    public TextMesh DamageReport; // A Text Mesh generates 3D geometry that display text string. Ref: https://docs.unity3d.com/Manual/class-TextMesh.html
+    public float Damage = 5; // This variable will be shown in the TextMesh
+    Color txtColor; // Used to modify the alpha value of the TextMesh 
+                    // This will allow us to turn on/off the TextMesh object withough having to instanstiate it
+            // Next 3 are used for creating a timer to pick when we want to show or hide the damage report
+    public float SpawnTime = 2; 
+    public float KillTime = 3;
+    public float PreviousTime = 0;
+    bool HasChanged = false; // Checks if we received damage or not
+
+	// Use this for initialization
+	void Start () {
+        OrigScale = HealthBar.transform.localScale;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentBarLength = currentHealth / maximumHealth;
+        HealthBar.transform.LookAt(Camera.main.transform); // Our Health Bar quad will look at our camera, thus we will always see it in 3D
+
+        // This is used for testing
+        // Everytime you press left mouse button the HP bar will drop
+        if(Input.GetKey("Fire1"))
+        {
+            currentHealth -= 1.00f;
+            ChangeBar();
+        }
+	}
+
+    // Vector3.Lerp reference
+    // https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
+    void ChangeBar()
+    {
+        HealthBar.transform.localScale = Vector3.Lerp(OrigScale, new Vector3(currentBarLength, OrigScale.y, OrigScale.z), Time.time);
+    }
+}
